25b49137f92cafa2ca2d626920566e73037f19a1
“nishanthan-k” <krn.nishanthan@gmail.com>
2024-01-03 13:16:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import parse from "html-react-parser";
+import React from "react";
+import "./App.scss"
+import resumeData from "./assets/resume.json";
+
+function App() {
+  let imgData = [];
+  const dynamicConvertion = (node, index) => {
+    if (node.attribs && node.attribs.id === "profile-info") {
+      return (
+        <p className="student-name" key={index}>
+          {node.children[0].children[0].data}
+        </p>
+      );
+    } else if (node.attribs && node.attribs.id === "social-links") {
+      return (
+        <div className="links">
+          <a className="link" href={node.children[0].attribs.href} key={index}>
+            {node.children[0].children[0].data}
+          </a>
+          <a className="link" href={node.children[0].attribs.href} key={index}>
+            {node.children[1].children[0].data}
+          </a>
+        </div>
+      );
+    } else if (node.attribs && node.attribs.id === "profile-img") {
+      // console.log(node);
+      imgData.push(node.children[0], node.children[1]);
+      return (
+        // <div className="profile-img-container">
+        //   <img
+        //     className="profile-img-1"
+        //     src={node.children[0].attribs.src}
+        //     alt={node.children[0].attribs.alt}
+        //     // src={src[0]}
+        //     // alt={node.children[0].attribs.alt}
+        //     width={100}
+        //   />
+        //   <img
+        //     className="profile-img-2"
+        //     src={node.children[1].attribs.src}
+        //     alt={node.children[1].attribs.alt}
+        //     // src={src[1]}
+        //     // alt={node.children[1].attribs.alt}
+        //     width={100}
+        //   />
+        // </div>
+
+        <div className="imgData">
+          <img
+            className="imgData-1"
+            src={imgData[0].attribs.src}
+            alt={imgData[0].attribs.alt}
+            width={100}
+          />
+          <img
+            className="imgData-2"
+            src={imgData[1].attribs.src}
+            alt={imgData[1].attribs.alt}
+            width={100}
+          />
+        </div>
+      );
+    }
+  };
+
+  const reactElements = parse(resumeData.content, {
+    replace: dynamicConvertion,
+  });
+
+  return <div className="App">{reactElements}</div>;
+}
+
+export default App;
